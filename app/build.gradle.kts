d43ca3b3ac91095ffb433e510819d7aaa26d2a00
gustavo <gustavo.isaac@gmail.com>
2024-02-01 05:49:08 +0000

    room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.com.google.devtools.ksp)
 }
 
 android {
@@ -41,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.6"
     }
     packaging {
         resources {
@@ -63,6 +64,11 @@ dependencies {
 
     implementation(libs.converter.moshi)
     implementation(libs.coil.compose)
+
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    annotationProcessor(libs.room.compiler)
+    ksp(libs.room.compiler)
 
     testImplementation(libs.junit)
 
