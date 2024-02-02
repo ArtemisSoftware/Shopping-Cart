245f39f816dd0cab93bc74e2d79d46bcaf924bef
gustavo <gustavo.isaac@gmail.com>
2024-02-02 12:15:49 +0000

    AddItemState

---
+++ app/src/main/java/com/artemissoftware/shoppingcart/presentation/additem/AddItemState.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.shoppingcart.presentation.additem
+
+data class AddItemState(
+    val name: String = "",
+    val amount: String = "",
+    val price: String = "",
+    val imageUrl: String = "",
+)
