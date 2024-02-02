6e64a3974a77debb45294d468668fe7275bba776
gustavo <gustavo.isaac@gmail.com>
2024-02-02 12:55:35 +0000

    ValidateItemUseCase

--- app/src/main/java/com/artemissoftware/shoppingcart/presentation/additem/AddItemEvents.kt
+++ app/src/main/java/com/artemissoftware/shoppingcart/presentation/additem/AddItemEvents.kt
@@ -1,6 +1,9 @@
 package com.artemissoftware.shoppingcart.presentation.additem
 
 sealed class AddItemEvents {
-
-    data class InsertItem(val name: String, val amount: String, val price: String, val imageUrl: String)
+    data class UpdateName(val name: String) : AddItemEvents()
+    data class UpdateAmount(val amount: String) : AddItemEvents()
+    data class UpdatePrice(val price: String) : AddItemEvents()
+    data class UpdateImageUrl(val imageUrl: String) : AddItemEvents()
+    object InsertItem : AddItemEvents()
 }
