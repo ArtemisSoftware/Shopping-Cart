7d86f86d0fc900513843de3e71cefbb1839ce319
gustavo <gustavo.isaac@gmail.com>
2024-02-05 06:25:54 +0000

    ShoppingCartRepositoryImplTest

---
+++ app/src/test/java/com/artemissoftware/shoppingcart/data/repositories/ShoppingCartRepositoryImplTest.kt
@@ -0,0 +1,34 @@
+package com.artemissoftware.shoppingcart.data.repositories
+
+import com.artemissoftware.shoppingcart.TestShoppingItemData.shoppingItem
+import com.artemissoftware.shoppingcart.TestShoppingItemData.shoppingItemEntity
+import com.artemissoftware.shoppingcart.data.database.ShoppingDao
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class ShoppingCartRepositoryImplTest {
+
+    private lateinit var repository: ShoppingCartRepositoryImpl
+    private lateinit var shoppingDao: ShoppingDao
+
+    @BeforeEach
+    fun setUp() {
+        shoppingDao = mockk()
+        repository = ShoppingCartRepositoryImpl(shoppingDao = shoppingDao)
+    }
+
+    @Test
+    fun `Inset item, check dao is called`() = runBlocking {
+        coEvery { shoppingDao.insertShoppingItem(shoppingItemEntity) } returns Unit
+
+        repository.insertShoppingItem(shoppingItem)
+
+        coVerify {
+            shoppingDao.insertShoppingItem(shoppingItemEntity)
+        }
+    }
+}
