c67c609efb14e401a88771302bb3f1be03ec5797
gustavo <gustavo.isaac@gmail.com>
2024-04-26 12:05:52 +0100

    New ui design

--- app/src/main/java/com/artemissoftware/shoppingcart/ui/theme/Type.kt
+++ app/src/main/java/com/artemissoftware/shoppingcart/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package com.artemissoftware.shoppingcart.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.artemissoftware.shoppingcart.R
+
+val RegularFont = FontFamily(Font(R.font.railyway_regular))
+val MediumFont = FontFamily(Font(R.font.raleway_medium))
+val SemiboldFont = FontFamily(Font(R.font.railyway_regular))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
