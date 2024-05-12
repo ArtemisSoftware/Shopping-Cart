1f38999da840a17883a26478ae1f2735d798df04
gustavo <gustavo.isaac@gmail.com>
2024-05-12 19:19:06 +0100

    ApiKeyInterceptor

---
+++ app/src/main/java/com/artemissoftware/shoppingcart/data/network/interceptors/ApiKeyInterceptor.kt
@@ -0,0 +1,24 @@
+package com.artemissoftware.shoppingcart.data.network.interceptors
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class ApiKeyInterceptor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        // Modify the request URL to add the API key as a query parameter
+        val originalHttpUrl = originalRequest.url
+
+        val newHttpUrl = originalHttpUrl.newBuilder()
+            .addQueryParameter("key", "Constants.API_KEY") // TODO: The key and value for the query parameter
+            .build()
+
+        val newRequest = originalRequest.newBuilder()
+            .url(newHttpUrl)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+}
