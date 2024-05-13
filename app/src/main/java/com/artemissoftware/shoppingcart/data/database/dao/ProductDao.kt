59bfedfbb0f9317742ac402cb2d48c6332f30e47
gustavo <gustavo.isaac@gmail.com>
2024-05-13 09:23:23 +0100

    ProductDao

---
+++ app/src/main/java/com/artemissoftware/shoppingcart/data/database/dao/ProductDao.kt
@@ -0,0 +1,24 @@
+package com.artemissoftware.shoppingcart.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.artemissoftware.shoppingcart.data.database.entities.ProductEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ProductDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(productEntity: ProductEntity)
+
+    @Delete
+    suspend fun delete(productEntity: ProductEntity)
+
+    @Query("SELECT * FROM products")
+    fun getAll(): Flow<List<ProductEntity>>
+
+    @Query("SELECT SUM(price * amount) FROM products")
+    fun getTotalPrice(): Flow<Double>
+}
