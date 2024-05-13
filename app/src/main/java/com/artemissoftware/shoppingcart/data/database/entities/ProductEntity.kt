1e4daf2db8e368e87017f8e92f8ed9e3a66d8921
gustavo <gustavo.isaac@gmail.com>
2024-05-13 09:20:35 +0100

    ProductEntity

---
+++ app/src/main/java/com/artemissoftware/shoppingcart/data/database/entities/ProductEntity.kt
@@ -0,0 +1,14 @@
+package com.artemissoftware.shoppingcart.data.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class ProductEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val amount: Int,
+    val price: Double,
+    val imageUrl: String,
+)
