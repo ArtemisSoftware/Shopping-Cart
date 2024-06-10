4a0126c282a9f893b15ad4b81d4a4691df300be3
gustavo <gustavo.isaac@gmail.com>
2024-06-10 11:07:02 +0100

    TestActivity

---
+++ app/src/main/java/com/artemissoftware/shoppingcart/test/TestActivity.kt
@@ -0,0 +1,12 @@
+package com.artemissoftware.shoppingcart.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TestActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
