61473c04e4096bd4f6ed607a9318fdc2204f398e
gustavo <gustavo.isaac@gmail.com>
2024-06-10 11:07:27 +0100

    ShoppingCartComposeRule

---
+++ app/src/main/java/com/artemissoftware/shoppingcart/test/ShoppingCartComposeRule.kt
@@ -0,0 +1,6 @@
+package com.artemissoftware.shoppingcart.test
+
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
+typealias ShoppingCartComposeRule = AndroidComposeTestRule<ActivityScenarioRule<TestActivity>, TestActivity>
