a729b1c74b0b45923affdf59818847d288ee75e9
gustavo <gustavo.isaac@gmail.com>
2024-06-10 11:10:47 +0100

    CartScreenTest refactor

--- app/src/androidTest/java/com/artemissoftware/shoppingcart/presentation/cart/CartScreenTest.kt
+++ app/src/androidTest/java/com/artemissoftware/shoppingcart/presentation/cart/CartScreenTest.kt
@@ -1,18 +1,11 @@
 package com.artemissoftware.shoppingcart.presentation.cart
 
-import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onChildAt
-import androidx.compose.ui.test.onNodeWithTag
 import com.artemissoftware.shoppingcart.InstrumentedProductTestData.productEntity
-import com.artemissoftware.shoppingcart.MainActivity
-import com.artemissoftware.shoppingcart.presentation.cart.TestTags.CART_LIST
-import com.artemissoftware.shoppingcart.presentation.cart.TestTags.CART_TOTAL_BAR
-import com.artemissoftware.shoppingcart.presentation.cart.TestTags.TOTAL_VALUE
 import com.artemissoftware.shoppingcart.test.ShoppingCartAndroidTest
+import com.artemissoftware.shoppingcart.test.TestActivity
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
@@ -20,34 +13,30 @@ import org.junit.Test
 @HiltAndroidTest
 class CartScreenTest: ShoppingCartAndroidTest(){
 
-
     @get:Rule
-    val composeRule = createAndroidComposeRule<MainActivity>()
+    val composeRule = createAndroidComposeRule<TestActivity>()
 
     @Test
     fun load_cart_check_it_has_products_and_total_is_correct() = runBlocking<Unit>{
 
-        db.getProductDao().insert(productEntity)
-        val total = productEntity.price * productEntity.amount
+        val total = setCartProducts()
+
+        composeRule.setContent {
+            CartScreen(
+                navigateToDetail = {},
+                navigateToSearchProduct = {}
+            )
+        }
+
         composeRule.awaitIdle()
 
-        val cartList = composeRule
-            .onNodeWithTag(CART_LIST)
+        CartScreenRobot(composeRule)
+            .assertCartListIsDisplayed()
+            .assertCartTotalIsDisplayed(total = total)
+    }
 
-        cartList
-            .assertIsDisplayed()
-
-        cartList
-            .onChildAt(0)
-            .assertIsDisplayed()
-
-        composeRule
-            .onNodeWithTag(CART_TOTAL_BAR)
-            .assertIsDisplayed()
-
-        composeRule
-            .onNodeWithTag(TOTAL_VALUE)
-            .assertIsDisplayed()
-            .assertTextEquals(total.toString())
+    private suspend fun setCartProducts(): Double{
+        db.getProductDao().insert(productEntity)
+        return db.getProductDao().getTotalPrice().first()
     }
 }
