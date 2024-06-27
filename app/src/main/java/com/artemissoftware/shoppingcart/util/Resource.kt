09f983c7460d37f51444e1d91793b2b2b8563e61
gustavo <gustavo.isaac@gmail.com>
2024-06-27 12:46:21 +0100

    More merge clean up

--- app/src/main/java/com/artemissoftware/shoppingcart/util/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.artemissoftware.shoppingcart.util
-
-sealed class Resource<T>(val value: T? = null, val message: String?) {
-    data class Success<T>(val data: T? = null) : Resource<T>(value = data, message = null)
-    data class Error<T>(val msg: String, val data: T?) : Resource<T>(value = data, message = msg)
-    data class Loading<T>(val data: T? = null) : Resource<T>(value = data, message = null)
-}
--- app/src/main/java/com/artemissoftware/shoppingcart/util/constants/ValidationConstants.kt
+++
@@ -1,6 +0,0 @@
-package com.artemissoftware.shoppingcart.util.constants
-
-object ValidationConstants {
-    const val MAX_NAME_LENGTH = 20
-    const val MAX_PRICE_LENGTH = 10
-}
